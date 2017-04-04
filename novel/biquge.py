32c70aa1c8d09e4ed802a3c4d2abb23a07baf66e
honmaple <xiyang0807@gmail.com>
2017-04-04 22:45:44 +0800

    小说爬虫 add 线程池

--- novel/biquge.py
+++ novel/biquge.py
@@ -6,14 +6,52 @@
 # Author: jianglin
 # Email: xiyang0807@gmail.com
 # Created: 2017-04-04 16:58:39 (CST)
-# Last Update:星期二 2017-4-4 18:25:39 (CST)
+# Last Update:星期二 2017-4-4 22:45:15 (CST)
 #          By:
 # Description:
 # **************************************************************************
 from bs4 import BeautifulSoup
 from lxml import html
-import requests
 from random import choice
+from queue import Queue
+import threading
+import requests
+
+
+class ThreadPool(object):
+    '''线程池实现'''
+
+    def __init__(self, num):
+        self.num = num
+        self.queue = Queue()
+        self.threads = []
+
+    def wait(self):
+        self.queue.join()
+
+    def get_thread(self):
+        return self.queue.get()
+
+    def add_thread(self, target, args=()):
+        self.queue.put((target, args))
+
+    def create_thread(self):
+        for t in range(self.num):
+            self.threads.append(SpiderThread(self.queue))
+
+
+class SpiderThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.setDaemon(True)
+        self.start()
+
+    def run(self):
+        while not self.queue.empty():
+            call, args = self.queue.get()
+            call(*args)
+            self.queue.task_done()
 
 
 class Spider(object):
@@ -39,6 +77,9 @@ class Spider(object):
         r = requests.get(url, headers=self.headers)
         return r.text
 
+    def thread(self, title, url):
+        self.parse_content(title, self.request(url))
+
     def parse(self, content):
         selector = html.fromstring(content)
         hrefs = selector.xpath('//ul[@class="chapter"]/li/a')
@@ -46,7 +87,7 @@ class Spider(object):
             title = href.xpath('text()')[0]
             url = self.url_prefix + href.xpath('@href')[0]
             print(title, url)
-            self.parse_content(title, self.request(url))
+            pool.add_thread(self.thread, args=(title, url))
         next_page = selector.xpath(
             '//div[@class="page"]/a[text()="下一页"]/@href')
         if next_page:
@@ -59,14 +100,18 @@ class Spider(object):
         selector = html.fromstring(content)
         text = selector.xpath('//div[@class="nr_nr"]/div[@id="nr1"]/text()')
         text = '\n'.join(text)
-        with open('1.txt', 'a') as f:
+        with open('斗破苍穹.txt', 'a') as f:
             text = text.replace('\xa0', ' ')
             f.writelines('\n\n{}\n\n{}'.format(title, text))
+        print(title, 'spider done.')
 
     def start(self):
         return self.parse(self.request(self.start_url))
 
 
 if __name__ == '__main__':
-    spider = Spider("http://m.biqugetw.com/4/4571/")
+    pool = ThreadPool(20)
+    spider = Spider("http://m.biqugetw.com/10/10338/")
     spider.start()
+    pool.create_thread()
+    pool.wait()
